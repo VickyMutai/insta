0bb3f1b1fad9af3e1f925e342801d6181538c944
VickyMutai <vicky.mutai96@gmail.com>
2018-03-02 10:52:57 +0300

    setting up files

---
+++ insta/gram/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse,Http404
+
+# Create your views here.
+def home(request):
+    title = 'instagram'
+    test =  'Awesomeness'
+    return render(request,'index.html',{"title":title,
+                                        "test":test})
