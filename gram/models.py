c63110beb5a23567bb4b47566af7a0ff4cf658af
VickyMutai <vicky.mutai96@gmail.com>
2018-03-02 12:40:12 +0300

    removing commenta and likes columns

--- gram/models.py
+++ gram/models.py
@@ -4,11 +4,9 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     caption = models.CharField(max_length = 60)
-    comments = models.CharField(max_length= 60)
-    likes = models.CharField(max_length=30)
     upload_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.image
+        return self.caption
     class Meta:
         ordering = ['upload_date']
