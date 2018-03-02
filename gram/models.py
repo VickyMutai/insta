2d915b358463d1ec2c158ec55e264892bbe04be0
VickyMutai <vicky.mutai96@gmail.com>
2018-03-02 19:55:21 +0300

    create model for comment and migrate

--- gram/models.py
+++ gram/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    caption = models.CharField(max_length = 60)
-    upload_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.caption
-    class Meta:
-        ordering = ['upload_date']
 
 class Profile(models.Model):
     profilePic = models.ImageField(upload_to='profile/',blank=True,null=True)
@@ -19,3 +10,24 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.bio
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    caption = models.CharField(max_length = 60)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(blank=True, null=True)
+    profile = models.ForeignKey(Profile)
+
+    def __str__(self):
+        return self.caption
+    class Meta:
+        ordering = ['upload_date']
+
+class Comment(models.Model):
+    comments = models.CharField(max_length=60,blank=True,null=True)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    image = models.ForeignKey(Image)
+
+    def __str__(self):
+        return self.comments
--- gram/urls.py
+++ gram/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.home,name='home'),
     url(r'^profile/',views.profile, name='profile'),
+    url(r'^settings/',views.settings,name='settings'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
