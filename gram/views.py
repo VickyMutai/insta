af8166aa232ecf5aae750928b489bbe50f787a07
VickyMutai <vicky.mutai96@gmail.com>
2018-03-02 19:19:09 +0300

    display data to profile page

--- gram/views.py
+++ gram/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,Http404
-from .models import Image
+from .models import Image,Profile
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home(request):
-    title = 'Instagram'
+    title = 'Insta-Gram'
     test =  'Awesomeness'
     image = Image.objects.all()
     return render(request,'index.html',{"title":title,
@@ -14,4 +14,6 @@ def home(request):
                                         "images":image,})
 
 def profile(request):
-    return render(request,'profile/profile.html')
+    title = 'Insta-Gram'
+    profile = Profile.objects.all()
+    return render(request,'profile/profile.html',{"title":title,"profile":profile})
