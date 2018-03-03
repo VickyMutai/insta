d853151a7c3f63469ef5e2bcf130ac149587b8e9
VickyMutai <vicky.mutai96@gmail.com>
2018-03-03 14:41:09 +0300

    method for updating caption

--- gram/models.py
+++ gram/models.py
@@ -41,6 +41,11 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()
 
+    @classmethod
+    def update_caption(cls,id,caption):
+        captioned = Image.objects.filter(id=id).update(caption = caption)
+        return captioned
+
 class Comment(models.Model):
     comments = models.CharField(max_length=60,blank=True,null=True)
     comment_date = models.DateTimeField(auto_now_add=True)
