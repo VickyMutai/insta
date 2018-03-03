68a899d23aa2df78de96d56e65e0e56b444b4a5c
VickyMutai <vicky.mutai96@gmail.com>
2018-03-03 06:03:40 +0300

    setting up models for testing

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Image,Profile,Comment
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.vicky=Profile(bio="I am awesome",user=self.vicky)
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.car=Image(caption="legacy goals",likes=200,profile=self.vicky)
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.love=Comment(comments="I love! All the best",user=self.vicky,image=self.car)
+        
