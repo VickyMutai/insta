945faa87c52566ab52f71205fbe4b097579e6067
VickyMutai <vicky.mutai96@gmail.com>
2018-03-03 18:55:09 +0300

    create form for editing user profile

--- gram/urls.py
+++ gram/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url('^$',views.home,name='home'),
     url(r'^profile/',views.profile, name='profile'),
     url(r'^settings/',views.settings,name='settings'),
+    url(r'^edit/',views.edit,name='edit'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- gram/views.py
+++ gram/views.py
@@ -19,14 +19,6 @@ def home(request):
 def profile(request):
     title = 'Insta-Gram'
     profile = Profile.get_profile()
-    return render(request,'profile/profile.html',{"title":title,
-                                                "profile":profile,})
-
-
-@login_required(login_url='/accounts/login/')
-def settings(request):
-    title = 'Insta-Gram'
-    settings = Profile.get_profile()
     current_user = request.user
     if request.method == 'POST':
         form = EditProfileForm(request.POST,request.FILES)
@@ -36,7 +28,29 @@ def settings(request):
             update.save()
     else:
         form = EditProfileForm()
-        
+    return render(request,'profile/profile.html',{"title":title,
+                                                  "form":form,
+                                                  "profile":profile,})
+
+
+@login_required(login_url='/accounts/login/')
+def settings(request):
+    title = 'Insta-Gram'
+    settings = Profile.get_profile()
     return render(request,'profile/settings.html',{"settings":settings,
-                                                    "title":title,
-                                                    "form":form,})
+                                                    "title":title,})
+
+@login_required(login_url='/accounts/login/')
+def edit(request):
+    title = 'Insta-Gram'
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            update = form.save(commit=False)
+            update.user = current_user
+            update.save()
+    else:
+        form = EditProfileForm()
+    return render(request,'profile/edit.html',{"title":title,
+                                                "form":form})
+
